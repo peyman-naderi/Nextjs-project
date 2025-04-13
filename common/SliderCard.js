86a6f843a9d6a42cae3d4dc7609da16a51a96cd2
peyman naderi <peyman.developer.01@gmail.com>
2025-04-14 01:37:01 +0330

    Optimize the Slider Card Component

---
+++ common/SliderCard.js
@@ -0,0 +1,13 @@
+// common/SliderCard.js
+import Image from 'next/image'
+
+export default function SliderCard({ imgsrc, title }) {
+  return (
+    <div className="bg-white border-2 mx-2 border-solid shadow-xl/70 flex flex-col rounded-3xl text-center h-full w-44">
+      <div className="w-full h-36 relative mb-4">
+        <Image src={imgsrc} fill className="object-cover rounded-t-3xl" alt={title} />
+      </div>
+      <h3 className="font-semibold mr-2 mb-4 text-right text-base">{title}</h3>
+    </div>
+  )
+}
--- components/slidercards/SliderCards.js
+++ components/slidercards/SliderCards.js
@@ -2,34 +2,34 @@
 
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
-import Image from 'next/image'
+import SliderCard from '@/common/SliderCard'
 
 const steps = [
-  { id: 1, title: 'گام ۱', img: '/p1.png' },
-  { id: 2, title: 'گام ۲', img: '/p2.png' },
-  { id: 3, title: 'گام ۳', img: '/p3.png' },
-  { id: 4, title: 'گام ۴', img: '/p4.png' },
-  { id: 5, title: 'گام ۵', img: '/p5.png' },
-  { id: 6, title: 'گام ۶', img: '/p6.png' },
-  { id: 7, title: 'گام ۷', img: '/p1.png' },
-  { id: 8, title: 'گام ۸', img: '/p2.png' },
-  { id: 9, title: 'گام ۹', img: '/p3.png' },
-  { id: 10, title: 'گام ۱۰', img: '/p4.png' },
+  { title: 'گام ۱', img: '/p1.png' },
+  { title: 'گام ۲', img: '/p2.png' },
+  { title: 'گام ۳', img: '/p3.png' },
+  { title: 'گام ۴', img: '/p4.png' },
+  { title: 'گام ۵', img: '/p5.png' },
+  { title: 'گام ۶', img: '/p6.png' },
+  { title: 'گام ۷', img: '/p1.png' },
+  { title: 'گام ۸', img: '/p2.png' },
+  { title: 'گام ۹', img: '/p3.png' },
+  { title: 'گام ۱۰', img: '/p4.png' },
 ]
 
 export default function SliderCards() {
   const [sliderRef] = useKeenSlider({
     loop: false,
     slides: {
-      perView: 2, 
-      spacing: 6, 
+      perView: 2.2,
+      spacing: 6,
     },
     breakpoints: {
       '(min-width: 640px)': {
-        slides: { perView: 4, spacing: 4 },
+        slides: { perView: 4.5, spacing: 4 },
       },
       '(min-width: 1024px)': {
-        slides: { perView: 6, spacing: 0 },
+        slides: { perView: 6.5, spacing: 0 },
       },
     },
   })
@@ -40,17 +40,13 @@ export default function SliderCards() {
         می‌خوام <span className="text-blue-600">وردپرس</span> کار حرفه‌ای بشم!
       </h2>
       <p className="text-center text-gray-600 mb-8">
-      لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است.</p>
+        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است.
+      </p>
 
       <div ref={sliderRef} className="keen-slider">
-        {steps.map((step) => (
-          <div key={step.id} className="keen-slider__slide flex justify-center ">
-            <div className="bg-white border-2 mx-2 border-solid  shadow-xl/70  flex flex-col rounded-3xl text-center h-full w-44">
-              <div className="w-full  h-36 relative mb-4">
-                <Image src={step.img} alt={step.title} fill className="object-cover rounded-t-3xl" />
-              </div>
-              <h3 className="font-semibold mr-2 mb-4 text-right text-base">{step.title}</h3>
-            </div>
+        {steps.map((step, index) => (
+          <div key={index} className="keen-slider__slide flex justify-center">
+            <SliderCard imgsrc={step.img} title={step.title} />
           </div>
         ))}
       </div>
