50b3da2f01959da5a14a551266b9aab400effd4c
peyman <peyman.developer.01@gmail.com>
2025-07-27 21:22:08 +0430

    Developing context and expanding the project

---
+++ context/CartShop.js
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const CartContext = createContext({
+  items: [],
+  getProductQuantity: () => {},
+  addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  deleteFromCart: () => {},
+  getTotlaAmount: () => {},
+});
